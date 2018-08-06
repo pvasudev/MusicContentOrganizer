fa0392071f5e7592378ac442088dfda3ce55eeed
Paavan Vasudev <paavanv@gmail.com>
2018-08-06 01:04:03 -0700

    Disabled downloading on some hosts.

--- src/main/java/net/paavan/music/content/organizer/downloader/DownloadManager.java
+++ src/main/java/net/paavan/music/content/organizer/downloader/DownloadManager.java
@@ -1,17 +1,22 @@
 package net.paavan.music.content.organizer.downloader;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.Set;
 
 public class DownloadManager {
+    private final boolean executeDownloader;
     private final Set<AlbumDownloader> albumDownloaders;
 
     @Inject
-    public DownloadManager(final Set<AlbumDownloader> albumDownloaders) {
+    public DownloadManager(@Named("execute.downloader") final boolean executeDownloader, final Set<AlbumDownloader> albumDownloaders) {
+        this.executeDownloader = executeDownloader;
         this.albumDownloaders = albumDownloaders;
     }
 
     public void download() {
-        albumDownloaders.forEach(AlbumDownloader::download);
+        if (executeDownloader) {
+            albumDownloaders.forEach(AlbumDownloader::download);
+        }
     }
 }
